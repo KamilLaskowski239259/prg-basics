e60e137c95261d24824a4871154a620277026092
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2024-10-23 21:17:49 +0000

    oct 23

---
+++ 02-TypesAndVariables/8-2.py
@@ -0,0 +1,6 @@
+r=int(input("Type in r of your circle: "))
+diameter=r*2
+circumference=diameter*3.14
+area=(r**2)*3.14
+print(f"The circunference is: {circumference}")
+print(f"The area is: {area}")
---
+++ 02-TypesAndVariables/8-3.py
@@ -0,0 +1,4 @@
+C=int(input("Enter temperature in Celcius: "))
+F=(C*0.125)+30
+K=C+273.15
+print(f"{C} Celcius is {F} Fahrenheit and {K} Kelvin")
