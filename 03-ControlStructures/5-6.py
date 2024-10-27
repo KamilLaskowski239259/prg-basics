4c13d2eb1620109e3a50ceb118407c7a2722249f
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2024-10-27 22:23:23 +0000

    Oct 27 Home

--- 03-ControlStructures/5-6.py
+++ 03-ControlStructures/5-6.py
@@ -7,7 +7,7 @@ sum_even = 0
 # Calculate the sum of even numbers
 i=1 
 while i <= N:
-    if i % 2 == 0:
+    if i % 2 == 0: 
         sum_even += i
-
+    i += 1
 print(f"The sum of even numbers from 1 to {N} is: {sum_even}")
