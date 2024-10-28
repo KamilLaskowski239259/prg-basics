81e6b2e86a64e1dcd84e57494b0c1ce6db244e16
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2024-10-28 23:39:03 +0000

    29 Oct Home

--- 03-ControlStructures/atm.py
+++ 03-ControlStructures/atm.py
@@ -3,33 +3,37 @@
 #
 balance = 1000  # Initial balance
 pin = '1111' # initial 4-digit PIN code
+ok = print(int(input("Enter your PIN: ")))
+if ok == pin:
+    
+    while True:
+        print()
+        print("ATM Menu:")
+        print("1. Check balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
 
-while True:
-    print()
-    print("ATM Menu:")
-    print("1. Check balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
+        choice = input("Choose an option (1-4): ")
+        print()
 
-    choice = input("Choose an option (1-4): ")
-    print()
-
-    if choice == '1':
-        print(f"Your current balance is: €{balance}")
-    elif choice == '2':
-        amount = float(input("Enter the amount to deposit: "))
-        balance += amount
-        print(f"€{amount} has been deposited. New balance: €{balance}")
-    elif choice == '3':
-        amount = float(input("Enter the amount to withdraw: "))
-        if amount <= balance:
-            balance -= amount
-            print(f"€{amount} has been withdrawn. New balance: €{balance}")
+        if choice == '1':
+            print(f"Your current balance is: €{balance}")
+        elif choice == '2':
+            amount = float(input("Enter the amount to deposit: "))
+            balance += amount
+            print(f"€{amount} has been deposited. New balance: €{balance}")
+        elif choice == '3':
+            amount = float(input("Enter the amount to withdraw: "))
+            if amount <= balance:
+                balance -= amount
+                print(f"€{amount} has been withdrawn. New balance: €{balance}")
+            else:
+                print("Insufficient balance.")
+        elif choice == '4':
+            print("Exiting... Thank you for using the ATM!")
+            break  # Exit the loop
         else:
-            print("Insufficient balance.")
-    elif choice == '4':
-        print("Exiting... Thank you for using the ATM!")
-        break  # Exit the loop
-    else:
-        print("Invalid option. Please try again.")
+            print("Invalid option. Please try again.")
+else:
+    print("Wrong PIN!!!")
