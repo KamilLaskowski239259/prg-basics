307e889279b622f6a24bd339799d781e88dcd1b8
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2024-11-23 18:19:55 +0000

    nov 23 home

--- 07-Arrays/1-10.py
+++ 07-Arrays/1-10.py
@@ -8,23 +8,24 @@ test_results = [
 ]
 
 # calculates the number of test questions
-question number = len(test_results)
+question_number = len(test_results)
 
 # calculates the number of correct answers
 correct_answers = 0
+incorrect_answers = 0
 for answer in test_results:
-   if ...:
-      correct_answer = ...
-
+   if answer == True:
+      correct_answers = correct_answers+1
 # calculates the number of incorrect answers
-...
+   elif answer == False:
+      incorrect_answers=incorrect_answers+1
 
 # calculates the percentage of correct answers
-...
+percent=(correct_answers/question_number)*100
 
 print('TEST STATISTICS')
 print('===============')
-print('Number of questions:', ...)
-print('Number of correct answers:', ...)
-...
-...
+print(f'Number of questions:', {question_number})
+print(f'Number of correct answers:', {correct_answers})
+print(f'number of incorrect answers:',{incorrect_answers})
+print(f'Percentage of correct answers',{percent})
