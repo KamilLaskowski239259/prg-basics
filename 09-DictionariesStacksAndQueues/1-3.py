eb5839ffc6a56898c5f383762541dde01928638d
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2024-12-06 11:38:30 +0000

    Dec 6 uni

---
+++ 09-DictionariesStacksAndQueues/1-3.py
@@ -0,0 +1,11 @@
+mobile = {
+"OS":"Android",
+"Brand":"Samsung",
+"Model":"S21 FE",
+"Storage":"128GB",
+"Color":"Black",
+"Camera":"48MP",
+"Number":"603249392"
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")
---
+++ 09-DictionariesStacksAndQueues/1-5.py
@@ -0,0 +1,10 @@
+countries = [
+{"name":"Poland", "population":38000000},
+{"name":"Japan", "population":160000000},
+{"name":"USA", "population":80324580714},
+{"name":"Russia", "population":9127456753},
+{"name":"Italy", "population":190234576398},
+]
+for i in countries:
+    print(i)
+    
