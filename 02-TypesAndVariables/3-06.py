edf04c4843dbe184ecd60ba43edf19a08cb7a636
KamilLaskowski239259 <239259@student.uek.krakow.pl>
2025-11-04 15:18:03 +0000

    nov 4 uni

---
+++ 02-TypesAndVariables/3-06.py
@@ -0,0 +1,5 @@
+import math
+d=(input("enter your height above the sea level: "))
+d=float(d)
+print("Your distance to the horizon is approximately ", 3.57*math.sqrt(d) ,"km" )
+      
---
+++ 02-TypesAndVariables/4-01.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = 21
+height = 1.75
+print(f"My name is {name}.")
+print(f"My age is {age}.")
+print(f"My height is {height}m.")
+print("In 6 years I will be", age + 6, "years old.")
